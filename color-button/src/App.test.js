1752bac4453bc090924442ac416efd2da573eb4b
Nastia Kuzina <ana@alteos.com>
2023-05-11 11:37:34 +0200

    Test and code function to transform camel case colors to spaced colors

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color, and updates when clicked', () => {
   render(<App />);
@@ -77,4 +78,18 @@ test('button turns from grey to blue by checkbox', () => {
 
   fireEvent.click(checkbox);
   expect(colorButton).toHaveStyle({"background-color": 'blue'});
+});
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  })
 });
