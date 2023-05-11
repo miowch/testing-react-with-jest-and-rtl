6972680a99f00591f3e90517c0b389a69573c90f
Nastia Kuzina <ana@alteos.com>
2023-05-11 19:46:48 +0200

    Test and code popover

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event'
 
 test('initial conditions', () => {
     render(<SummaryForm />);
@@ -11,17 +12,39 @@ test('initial conditions', () => {
     expect(confirmButton).toBeDisabled();
 });
 
-test('checkbox should enable button on first click and disable it on second one', () => {
+test('checkbox should enable button on first click and disable it on second one', async () => {
+    const user = userEvent.setup();
+
     render(<SummaryForm />);
 
     const checkbox = screen.getByRole('checkbox', {name: /terms and conditions/i});
     const confirmButton = screen.getByRole('button', {name: /confirm order/i});
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
 
     expect(confirmButton).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
 
     expect(confirmButton).toBeDisabled();
 });
+
+test('popover response to hover', async () => {
+    const user = userEvent.setup();
+    
+    render(<SummaryForm />);
+
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears on mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(popover).toBeInTheDocument();
+
+    // popover disappears when we mouse out
+    await user.unhover(termsAndConditions);
+    expect(popover).not.toBeInTheDocument();
+})
